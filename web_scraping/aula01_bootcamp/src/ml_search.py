9dcf491d1c73d71281a7c1fa5799200e6063334b
Guilherme Moreira <devguilhermemoreira@gmail.com>
2024-12-17 19:21:19 -0300

    ajustes ml_search.py

--- web_scraping/aula01_bootcamp/src/ml_search.py
+++ web_scraping/aula01_bootcamp/src/ml_search.py
@@ -1,27 +1,52 @@
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-keyword = "PS5"
 
-url = f"https://lista.mercadolivre.com.br/{keyword}"
+class MercadoLivreCrawler:
+    def execute_command(self, query):
+        url = f"https://lista.mercadolivre.com.br/{query.replace(' ', '-')}"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        }
 
-response = requests.get(url)
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
+            container = soup.find("ol", class_="ui-search-layout")
+            results = container.find_all("li", class_="ui-search-layout__item")
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, "html.parser")
+            data = []
+            for result in results:
+                link = None
+                title = result.find("h2", class_="poly-box").text.strip()
+                price = result.find(
+                    "span", class_="andes-money-amount__fraction"
+                ).text.strip()
+                link_tag = result.find("a")
+                if link_tag:
+                    link = link_tag.get("href")
+                data.append({"Produto": title, "Preço": price, "URL": link})
 
-    search_result = soup.find_all("div", class_="ui-search-result")
+            return data
+        else:
+            print("Falha ao fazer a solicitação HTTP.")
+            return None
 
-    data = []
+    def send_dataframe(self, query):
+        data = self.execute_command(query)
+        if data:
+            df = pd.DataFrame(data)
+            df.to_csv(f"aula01_bootcamp/data/{query}.csv", index=False)
+            return df
+        else:
+            return None
 
-    for result in search_result:
-        link = result.find("a", class_="ui-search-link").get("href")
-        title = result.find("h2", class_="ui-search-item__title").text
-        price = result.find("span", class_="price-tag-fraction").text
-        brand = result.find("span", class_="ui-search-item__group__element").text
 
-        data.append({"link": link, "title": title, "price": price, "brand": brand})
-
-    print(data)
+# Exemplo de utilização
+crawler = MercadoLivreCrawler()
+dataframe = crawler.send_dataframe("iphone 12")
+if dataframe is not None:
+    print(dataframe)
 else:
-    print(f"Error: {response.status_code}")
+    print("Erro ao obter os dados.")
