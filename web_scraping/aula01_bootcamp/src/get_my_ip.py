2318c430975a98519c309328b90738b9c5b62e24
Guilherme Moreira <devguilhermemoreira@gmail.com>
2024-12-18 20:02:57 -0300

    ajustes get_my_ip.py

--- web_scraping/aula01_bootcamp/src/get_my_ip.py
+++ web_scraping/aula01_bootcamp/src/get_my_ip.py
@@ -7,10 +7,12 @@ response = requests.get(url)
 
 # Verifica se a requisição foi bem-sucedida (código de status 200)
 if response.status_code == 200:
-    # Imprime o conteúdo da resposta (o endereço IP retornado pelo site)
-    print("ASN: ", response.json()["asn"].get("asnum"))
-    print("Organization: ", response.json()["asn"].get("org_name"))
-    print("City: ", response.json()["geo"].get("city"))
+    print(f'ASN: {response.json()["asn"].get("asnum")}')
+    print(f'Organization: {response.json()["asn"].get("org_name")}')
+    print(f'City: {response.json()["geo"].get("city")}')
+    print(
+        f'State: {response.json()["geo"].get("region_name")}-{response.json()["geo"].get("region")}'
+    )
 else:
     # Se a requisição não foi bem-sucedida, imprime o código de status
     print("Falha na requisição. Código de status: ", response.status_code)
